99cea004f341236538954ae8241c7fe438cddd9d
Edel <edelmedina322@gmail.com>
2024-01-25 18:04:28 -0300

    Change logic to a Regex one

--- src/app/page.js
+++ src/app/page.js
@@ -6,25 +6,16 @@ export default function Home() {
   const [ogText, setOgText] = useState("");
   const [newText, setNewText] = useState("");
   const [currentModifier, setCurrentModifier] = useState("red");
-  const [currentSelection, setCurrentSelection] = useState("aaaaa");
   const [outputText, setOutputText] = useState("");
 
   //Variables
-  const modifiers = [{ name: "red", selected: [], replaceWith: `` }];
+  const modifiers = [{ name: "red", regex: /\{(.*?)\}/g}];
 
   //Functions
-  //addSelection handles the text to modify
   //addChanges handles to change to the selection
   //replaceText is a smaller scope function that gets called for every modifier
   //transformText is the main one, handling the whole modifiers array and passing em to replaceText. Also sets state
 
-  const addSelection = (target, selection) => {
-    //Finds the modifier from t he array and if the selection is not empty pushes it to the "selected" property
-    let targetObject = modifiers.find((modifier) => modifier.name === target);
-    if (selection.length > 0) {
-      targetObject.selected.push(selection);
-    }
-  };
   const addChanges = (target, changes) => {
     let targetObject = modifiers.find((modifier) => modifier.name === target);
     if (changes.length > 0) {
@@ -32,13 +23,11 @@ export default function Home() {
     }
   };
   const replaceText = (text, modifier) => {
-    console.log(modifier);
     //Smaller function that only makes changes on modifier level
-    let modifiedText = modifier.selected.reduce((accumulator, toReplace) => {
-      return accumulator.replace(toReplace, modifier.replaceWith);
-    }, text);
-    console.log(modifiedText, text);
-    return modifiedText;
+    console.log(modifier);
+    const { regex, replaceWith } = modifier;
+    if (replaceWith) return text.replaceAll(regex, replaceWith);
+    else return text;
   };
   const trasformText = (original, changesArray) => {
     //Applies all changes and sets state
@@ -47,7 +36,6 @@ export default function Home() {
       return replaceText(accumulator, modifier);
     }, copyText);
     setOutputText(newText);
-    console.log(newText);
   };
 
   //html
@@ -60,29 +48,7 @@ export default function Home() {
           className="original-text"
           value={ogText}
           onChange={(e) => setOgText(e.target.value)}
-          onMouseUp={() => {
-            let selection = window.getSelection().toString();
-            if (selection.length > 0) setCurrentSelection(selection);
-          }}
         />
-        <button
-          onClick={() => {
-            console.log(ogText);
-          }}
-        >
-          Save text
-        </button>
-        <div className="change-btns">
-          <button>Add all occurences</button>
-          or
-          <button
-            onClick={(e) => {
-              addSelection(currentModifier, currentSelection);
-            }}
-          >
-            Add selection
-          </button>
-        </div>
       </div>
 
       <div className="customizer">
