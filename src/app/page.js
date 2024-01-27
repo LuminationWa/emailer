04bafe81ac83cf30c2e183846c7edee4ef613273
Edel <edelmedina322@gmail.com>
2024-01-27 19:09:30 -0300

    Added modifier creation

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,29 @@ export default function Home() {
   const [ogText, setOgText] = useState("");
   const [newText, setNewText] = useState("");
   const [modifiers, setModifiers] = useState([
-    { name: "default", regex: /\{(.*?)\}/g },
+    { name: "default", regex: /\{(.*?)\}/g, pattern: `{ }` },
   ]);
   const [currentModifier, setCurrentModifier] = useState("default");
   const [outputText, setOutputText] = useState("");
 
   //Functions
+  //crateModifier is a factory functior modifiers
+  //pushModifier simply pushes that new modifier to state
   //addChanges creates a copy of the modifiers array and makes changes to the target object properties. Updates state after.
   //replaceText is a smaller scope function that gets called for every modifier
   //transformText is the main one, handling the whole modifiers array and passing em to replaceText. Also sets state
 
+  const createModifier = (name, regex, pattern) => {
+    return { name, regex, pattern };
+  };
+  const pushModifier = (name, regex, pattern) => {
+    //Should check for no duplicates
+    const newModifiers = [...modifiers];
+    const newMod = createModifier(name, regex, pattern);
+    newModifiers.push(newMod);
+    setModifiers(newModifiers);
+    console.log(modifiers);
+  };
   const addChanges = (target, changes) => {
     const newModifiers = [...modifiers];
     let targetObject = newModifiers.find(
@@ -58,13 +71,48 @@ export default function Home() {
           value={ogText}
           onChange={(e) => setOgText(e.target.value)}
         />
+        <button
+          onClick={() => {
+            pushModifier("texter", /\/(.*?)\//g);
+          }}
+        >
+          +
+        </button>
+      </div>
+
+      <div className="modifier-creation">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const name = document.getElementById("name").value;
+            const regexValue = document.getElementById("regex").value;
+            const regexSplit = regexValue.split(" ");
+            const regex = new RegExp(`${regexSplit[0]}(.*?)${regexSplit[1]}`, "g");
+            pushModifier(name, regex, regexValue);
+            console.log(modifiers);
+          }}
+        >
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" id="name" required />
+          <label htmlFor="regex">Pattern(separated by space):</label>
+          <input type="text" name="regex" id="regex" required />
+          <button>Create</button>
+        </form>
       </div>
 
       <div className="customizer">
         <h1>Customize</h1>
         <div className="modifiers-bar">
           {modifiers.map((modifier) => (
-            <button key={modifier}>{modifier.name}</button>
+            <button
+              key={modifier.name}
+              onClick={() => {
+                setCurrentModifier(modifier.name);
+              }}
+            >
+              {modifier.name}
+              {modifier.pattern}
+            </button>
           ))}
         </div>
         <input
